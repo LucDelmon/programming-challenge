f9ea0ffed5fa7905a9dc0b6d0220c6e269a18eb6
LucDelmon <luc.delmon@gmail.com>
2022-08-26 14:44:00 +0200

    feat: complete service to load models from a hash and adds spec

--- lib/services/data_loader_service.rb
+++ lib/services/data_loader_service.rb
@@ -7,12 +7,48 @@ module XmlParser
   class DataLoaderService
     # @param [Hash] data_hash
     def initialize(data_hash)
-      @data_hash= data_hash
+      @data_hash = data_hash
     end
 
     # @return [Array]
     def call
-      []
+      load_search_result(@data_hash['SearchResults']['SearchResult'])
+    end
+
+    private
+
+    # @param [Array] search_result_array
+    # @return [Array]
+    def load_search_result(search_result_array)
+      search_result_array.map do |search_result|
+        XmlParser::SearchResult.new(
+          id: search_result['ID'],
+          connections: load_connections(search_result['Connections']['Connection'])
+        )
+      end
+    end
+
+    # @param [Array] connection_array
+    # @return [Array]
+    def load_connections(connection_array)
+      connection_array.map do |connection|
+        XmlParser::Connection.new(
+          start: connection['Start'],
+          finish: connection['Finish'],
+          departure_time: connection['DepartureTime'],
+          arrival_time: connection['ArrivalTime'],
+          train_name: connection['TrainName'],
+          fares: load_fares(connection['Fares']['Fare'])
+        )
+      end
+    end
+
+    # @param [Array] fare_array
+    # @return [Array]
+    def load_fares(fare_array)
+      fare_array.map do |fare|
+        XmlParser::Fare.new(name: fare['Name'], price: fare['Price']['Value'].to_f)
+      end
     end
   end
 end
---
+++ spec/services/data_loader_service_spec.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+RSpec.describe XmlParser::DataLoaderService do
+  let(:data_hash) do
+    {
+      'SearchResults' => {
+        'SearchResult' => [
+          {
+            'ID' => 'first_id',
+            'Connections' => {
+              'Connection' => [
+                {
+                  'Start' => 'first_start',
+                  'Finish' => 'first_finish',
+                  'DepartureTime' => 'first_departure_time',
+                  'ArrivalTime' => 'first_arrival_time',
+                  'TrainName' => 'first_train_name',
+                  'Fares' => {
+                    'Fare' => [
+                      {
+                        'Name' => 'first_fare_name',
+                        'Price' => { 'Currency' => 'GBP', 'Value' => '11.1' }
+                      },
+                      {
+                        'Name' => 'second_fare_name',
+                        'Price' => { 'Currency' => 'GBP', 'Value' => '22.2' }
+                      }
+                    ]
+                  }
+                },
+                {
+                  'Start' => 'second_start',
+                  'Finish' => 'second_finish',
+                  'DepartureTime' => 'second_departure_time',
+                  'ArrivalTime' => 'second_arrival_time',
+                  'TrainName' => 'second_train_name',
+                  'Fares' => {
+                    'Fare' => [
+                      {
+                        'Name' => 'third_fare_name',
+                        'Price' => { 'Currency' => 'GBP', 'Value' => '33.3' }
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          },
+          {
+            'ID' => 'second_id',
+            'Connections' => {
+              'Connection' => [
+                {
+                  'Start' => 'third_start',
+                  'Finish' => 'third_finish',
+                  'DepartureTime' => 'third_departure_time',
+                  'ArrivalTime' => 'third_arrival_time',
+                  'TrainName' => 'third_train_name',
+                  'Fares' => {
+                    'Fare' => [
+                      {
+                        'Name' => 'fourth_fare_name',
+                        'Price' => { 'Currency' => 'GBP', 'Value' => '44.4' }
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  end
+  let(:service) { described_class.new(data_hash) }
+  let(:service_call) { service.call }
+
+  it 'loads the data into the correct models' do
+    expect(service.call).to be_an_instance_of(Array)
+    aggregate_failures do
+      expect(service.call.first).to be_an_instance_of(XmlParser::SearchResult)
+      first_search_result = service_call.first
+      expect(first_search_result.id).to eq('first_id')
+      expect(first_search_result.connections.first).to be_an_instance_of(XmlParser::Connection)
+      first_connection = first_search_result.connections.first
+      expect(first_connection.start).to eq 'first_start'
+      expect(first_connection.start).to eq 'first_start'
+      expect(first_connection.finish).to eq 'first_finish'
+      expect(first_connection.departure_time).to eq 'first_departure_time'
+      expect(first_connection.arrival_time).to eq 'first_arrival_time'
+      expect(first_connection.train_name).to eq 'first_train_name'
+      expect(first_connection.fares.first).to be_an_instance_of(XmlParser::Fare)
+      expect(first_connection.fares.first.name).to eq 'first_fare_name'
+      expect(first_connection.fares.first.price).to eq 11.1
+      expect(first_connection.fares[1]).to be_an_instance_of(XmlParser::Fare)
+      expect(first_connection.fares[1].name).to eq 'second_fare_name'
+      expect(first_connection.fares[1].price).to eq 22.2
+      expect(first_search_result.connections[1]).to be_an_instance_of(XmlParser::Connection)
+      second_connection = first_search_result.connections[1]
+      expect(second_connection.start).to eq 'second_start'
+      expect(second_connection.finish).to eq 'second_finish'
+      expect(second_connection.departure_time).to eq 'second_departure_time'
+      expect(second_connection.arrival_time).to eq 'second_arrival_time'
+      expect(second_connection.train_name).to eq 'second_train_name'
+      expect(second_connection.fares.first).to be_an_instance_of(XmlParser::Fare)
+      expect(second_connection.fares.first.name).to eq 'third_fare_name'
+      expect(second_connection.fares.first.price).to eq 33.3
+      expect(service.call[1]).to be_an_instance_of(XmlParser::SearchResult)
+      second_search_result = service_call[1]
+      expect(second_search_result.connections.first).to be_an_instance_of(XmlParser::Connection)
+      expect(second_search_result.id).to eq('second_id')
+      third_connection = second_search_result.connections.first
+      expect(third_connection.start).to eq 'third_start'
+      expect(third_connection.finish).to eq 'third_finish'
+      expect(third_connection.departure_time).to eq 'third_departure_time'
+      expect(third_connection.arrival_time).to eq 'third_arrival_time'
+      expect(third_connection.train_name).to eq 'third_train_name'
+      expect(third_connection.fares.first).to be_an_instance_of(XmlParser::Fare)
+      expect(third_connection.fares.first.name).to eq 'fourth_fare_name'
+      expect(third_connection.fares.first.price).to eq 44.4
+    end
+  end
+end

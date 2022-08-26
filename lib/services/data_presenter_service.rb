56c39feaecb0a3a7fe4386323c1e69627bf92a61
LucDelmon <luc.delmon@gmail.com>
2022-08-26 18:55:31 +0200

    feat: update presenter to use an ApiResult instead of an array. Also prepare and test the print of the quickest and cheapest results

--- lib/services/data_presenter_service.rb
+++ lib/services/data_presenter_service.rb
@@ -3,16 +3,18 @@
 module XmlParser
   # class XmlParser::DataPresenterService
   #
-  # class for printing a search result into the console
+  # class for printing an api_result into the console
   class DataPresenterService
-    # @param [Array<XmlParser::SearchResult>] search_results
-    def initialize(search_results)
-      @search_results = search_results
+    # @param [api_result] api_result
+    def initialize(api_result)
+      @api_result = api_result
     end
 
     # @return [nil]
     def call
-      @search_results.each do |search_result|
+      puts "Quickest: #{@api_result.quickest_result.id}"
+      puts "Cheapest: #{@api_result.cheapest_result.id}"
+      @api_result.search_results.each do |search_result|
         puts '------Search Result------'
         puts search_result.to_s
       end
--- spec/services/data_presenter_service_spec.rb
+++ spec/services/data_presenter_service_spec.rb
@@ -5,16 +5,19 @@ require 'rspec'
 RSpec.describe XmlParser::DataPresenterService do
   let(:first_search_result) { XmlParser::SearchResult.new(id: 'first_id', connections: []) }
   let(:second_search_result) { XmlParser::SearchResult.new(id: 'second_id', connections: []) }
+  let(:api_result) { XmlParser::ApiResult.new(search_results: [first_search_result, second_search_result]) }
 
   before do
     allow(first_search_result).to receive(:to_s).and_return('first_search_result_to_s')
     allow(second_search_result).to receive(:to_s).and_return('second_search_result_to_s')
+    allow(api_result).to receive(:quickest_result).and_return(instance_double(XmlParser::SearchResult, id: 'quickest_id'))
+    allow(api_result).to receive(:cheapest_result).and_return(instance_double(XmlParser::SearchResult, id: 'cheapest_id'))
   end
 
   it 'prints the search results in the console' do
-    search_results = [first_search_result, second_search_result]
-    presenter = described_class.new(search_results)
+    presenter = described_class.new(api_result)
     expect { presenter.call }.to output(
+      "Quickest: quickest_id\nCheapest: cheapest_id\n" \
       "------Search Result------\nfirst_search_result_to_s\n" \
       "------Search Result------\nsecond_search_result_to_s\n"
     ).to_stdout

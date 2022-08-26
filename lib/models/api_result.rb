2b6f66b077ef483439d70264d9fd84869ee96589
LucDelmon <luc.delmon@gmail.com>
2022-08-26 18:49:22 +0200

    feat: add new api_model result

---
+++ lib/models/api_result.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module XmlParser
+  # class XmlParser::SearchResult
+  #
+  # Represent an ApiResult
+  class ApiResult
+    attr_reader :search_results
+
+    # @param search_results [Array<XmlParser::SearchResult>]
+    def initialize(search_results:)
+      @search_results = search_results
+    end
+
+    def quickest_result; end
+
+    def cheapest_result; end
+  end
+end
